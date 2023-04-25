2ccd5362a333853d643700e053d13b5d64284fb6
Artem Kazakov <artemkazakov947@gmail.com>
2023-04-25 09:04:51 +0200

    Added rest_framework to INSTALLED_APPS in settings.py

--- simply_chat/settings.py
+++ simply_chat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "rest_framework_simplejwt",
     "user",
     "chat",
@@ -132,8 +133,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 
