90518bd1556e2fc4dcdfd995e02448719bf396e8
Artem Kazakov <artemkazakov947@gmail.com>
2023-04-28 10:41:25 +0200

    Added ThreadViewSet with get_serializer and get_queryset methods

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from chat.models import Thread
+from chat.serializers import ThreadSerializer, ThreadCreateSerializer
+
+
+class ThreadViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.RetrieveModelMixin,
+                    viewsets.GenericViewSet):
+    serializer_class = ThreadSerializer
+    queryset = Thread.objects.all()
+
+    def get_serializer_class(self):
+        serializers = {
+            "list": ThreadSerializer,
+            "create": ThreadCreateSerializer,
+            "retrieve": ThreadSerializer,
+        }
+        return serializers[self.action]
+
+    def get_queryset(self):
+        user = self.request.GET.get("user")
+
+        if self.request.user.is_staff:
+            if user:
+                return self.queryset.filter(participants=user)
+            else:
+                return self.queryset.filter(participants=self.request.user)
+        else:
+            return self.queryset.filter(participants=self.request.user)
