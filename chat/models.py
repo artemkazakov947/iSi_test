b0a9f78dfa00193641e74086d9a3127c9db283d8
Artem Kazakov <artemkazakov947@gmail.com>
2023-04-28 10:39:49 +0200

    Changed relation type in Thread model in participants field. Deleted save method in Thread model.

--- chat/models.py
+++ chat/models.py
@@ -1,25 +1,17 @@
 from django.conf import settings
 from django.db import models
-from rest_framework.exceptions import ValidationError
 
 
 class Thread(models.Model):
-    participants = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="threads")
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="threads")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Thread for {self.participants}. Updated: {self.updated}"
+        return f"Thread id {self.id}"
 
     class Meta:
         ordering = ["-updated"]
-
-    def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
-        if self.participants.count() > 2:
-            raise ValidationError("Thread can not have more then 2 participant")
-        return super(Thread, self).save(force_insert, force_update, using, update_fields)
 
 
 class Message(models.Model):
